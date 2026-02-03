c21215cacbc8313c2387472f035561bf9091d4ad
Ashikanparasu <ashikanparasu@gmail.com>
2026-02-03 09:11:46 +0530

    Basic2

--- Python/basic.py
+++ Python/basic.py
@@ -14,3 +14,19 @@ Age = 20
 
 total_price = 100
 full_name = "Ashik A"
+
+
+#type_conversion
+#type_casting
+
+ans = int(5+10.0)
+print(ans,type(ans))
+
+#user_input
+
+a = int(input("Enter The 1st Number:"))
+b = int(input("Enter The 2nd Number:"))
+
+avg = (a+b)/2
+
+print(avg)
